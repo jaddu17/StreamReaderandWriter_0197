eccb324fd406ef194026b3ea4da37498c27d4973
jaddu17 <shafajaddu@gmail.com>
2024-06-04 10:55:58 +0700

    membuat exception dengan manual

---
+++ manualException/manualException.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        // throw 0,5; // melemparkan sebuah intergar maka 
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    catch (int a)
+    {
+        // blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...)
+    {
+        // jika selain integar maka block ini akan dieksekusi
+        cout << "default Pengecualian dieksekusi" << endl;
+    }
+    
+    return 0;
+}
