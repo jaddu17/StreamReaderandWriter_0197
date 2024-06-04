66b02e504e7cd05a89afc02115740569b6aff101
jaddu17 <shafajaddu@gmail.com>
2024-06-04 10:37:35 +0700

    membuka file dengan path

---
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string baris;
+    string NamaFile;
+
+    cout << "Masukkan Nama File : ";
+    cin >> NamaFile;
+
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+}
